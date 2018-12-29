8d44bc16e549b4d5a055aa1759d94a68a0812540
KlintsovAleks <klintsovaleksandr@gmail.com>
2018-12-29 23:32:00 +0300

    Загрузил моё портфолио

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Секция на весь экран
+// $(document).ready(function(){
+// 	$('section').height($(window).height());
+// })
+
+// // PageScroll2id
+$(document).ready(function(){
+  $(".nav-link").mPageScroll2id({
+  	offset: 150,
+  });
+});
